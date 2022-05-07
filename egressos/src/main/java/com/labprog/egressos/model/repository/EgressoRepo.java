ca08216781c2130b0ca94ebca5e4aefd476f45b5
Luigi Dovera <luigidovera11@gmail.com>
2022-05-07 10:44:54 -0300

    [Dev] - Luigi - Egresso feito, mas Lombok não funcionando, tags não geram getters, setters e builders

---
+++ egressos/src/main/java/com/labprog/egressos/model/repository/EgressoRepo.java
@@ -0,0 +1,21 @@
+package com.labprog.egressos.model.repository;
+
+import com.labprog.egressos.model.Egresso;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface EgressoRepo 
+    extends JpaRepository<Egresso, Long>{
+    
+        Egresso findByEmail(String email);
+        boolean existsByEmail(String email);
+
+        @Query("select e from egresso e where e.nome")
+        Egresso obterEgressoPorNome(
+            @Param("nomeEgresso") String nomeEgresso
+        );
+
+}
+
---
+++ egressos/src/test/java/com/labprog/egressos/model/EgressoRepositoryTest.java
@@ -0,0 +1,38 @@
+package com.labprog.egressos.model;
+
+import com.labprog.egressos.model.repository.EgressoRepo;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class EgressoRepositoryTest{
+    @Autowired
+    EgressoRepo repo;
+
+
+    @Test
+    public void deveSalvarEgresso(){
+        //cenário
+        Egresso novo = Egresso.builder().nome("tuludan")
+                                        .email("a@a.com")
+                                        .senha("123")
+                                        .build();
+        
+        //ação
+        Egresso retorno = repo.save(novo);
+
+        //verificação
+        Assertions.assertNotNull(retorno);
+        Assertions.assertEquals(novo.getNome(), retorno.getNome());
+        Assertions.assertEquals(novo.getEmail(), retorno.getEmail());
+        Assertions.assertEquals(novo.getCpf(), retorno.getCpf());
+        Assertions.assertEquals(novo.getResumo(), retorno.getResumo());
+        Assertions.assertEquals(novo.getUrl_foto(), retorno.getUrl_foto());
+    }
+
+}
