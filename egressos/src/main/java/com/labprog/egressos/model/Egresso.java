283df7af7499d8cd3ac925ee9cd034be5641b3bc
Luigi Dovera <luigidovera11@gmail.com>
2022-05-06 22:53:12 -0300

    [Dev] - Luigi - Egressos Model escrito, mas possível problema com lombok,  tests não funcionam por conta disso

--- egressos/src/main/java/com/labprog/egressos/model/Egresso.java
+++ egressos/src/main/java/com/labprog/egressos/model/Egresso.java
@@ -1,5 +1,42 @@
 package com.labprog.egressos.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="egressos")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Egresso {
-    
+    @Id
+    @Column(name = "id_egressos")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "nome")
+    private String nome;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "cpf")
+    private String cpf;
+
+    @Column(name = "resumo")
+    private String resumo;
+
+    @Column(name = "url_foto")
+    private String url_foto;
+
 }
