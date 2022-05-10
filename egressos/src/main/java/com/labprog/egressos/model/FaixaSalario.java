5c9b65a27a69247c9c6dada36530dbb40f2edbfd
Luigi Dovera <luigidovera11@gmail.com>
2022-05-10 17:42:54 -0300

    [Dev] - Luigi - Model FaixaSalario e mapeamento dele com ProfEgresso

--- egressos/src/main/java/com/labprog/egressos/model/FaixaSalario.java
+++ egressos/src/main/java/com/labprog/egressos/model/FaixaSalario.java
@@ -1,14 +1,14 @@
 package com.labprog.egressos.model;
 
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
-
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -16,37 +16,20 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "prof_egresso")
+@Table(name="faixa_salario")
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class ProfEgresso {
+public class FaixaSalario {
     @Id
-    @Column(name = "id_prof_egresso")
+    @Column(name = "id_faixa_salario")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
-    @ManyToOne
-    @JoinColumn(name = "egresso_id")
-    private Egresso egresso;
-
-    @ManyToOne
-    @JoinColumn(name = "cargo_id")
-    private Cargo cargo;
-
-    @ManyToOne
-    @JoinColumn(name = "faixa_salario_id")
-    private FaixaSalario faixaSalario;
-
-    @Column(name = "empresa")
-    private String empresa;
+    @OneToMany(mappedBy = "faixa_salario")
+    private List<ProfEgresso> profEgressos;
 
     @Column(name = "descricao")
     private String descricao;
-
-    @Column(name = "data_registro")
-    private Data dataRegistro;
-
-
-}    
+}
