7c80bd739914e201e7d78745af2c7e446c351aa7
Luigi Dovera <luigidovera11@gmail.com>
2022-05-10 06:36:18 -0300

    [Dev] - Luigi - Testes para Egresso finalizado

--- egressos/src/test/java/com/labprog/egressos/model/EgressoRepositoryTest.java
+++ egressos/src/test/java/com/labprog/egressos/model/EgressoRepositoryTest.java
@@ -1,5 +1,7 @@
 package com.labprog.egressos.model;
 
+import java.util.Optional;
+
 import com.labprog.egressos.model.repository.EgressoRepo;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -17,7 +19,7 @@ public class EgressoRepositoryTest {
     @Test
     public void deveSalvarEgresso() {
         // cenário
-        Egresso novo = Egresso.builder()
+        Egresso egresso = Egresso.builder()
                 .nome("tuludan")
                 .email("a@a.com")
                 .cpf("1234")
@@ -26,18 +28,71 @@ public class EgressoRepositoryTest {
                 .build();
 
         // ação
-        Egresso retorno = repo.save(novo);
+        Egresso retorno = repo.save(egresso);
 
         // verificação
         Assertions.assertNotNull(retorno);
-        Assertions.assertEquals(novo.getNome(), retorno.getNome());
-        Assertions.assertEquals(novo.getEmail(), retorno.getEmail());
-        Assertions.assertEquals(novo.getCpf(), retorno.getCpf());
-        Assertions.assertEquals(novo.getResumo(), retorno.getResumo());
-        Assertions.assertEquals(novo.getUrl_foto(), retorno.getUrl_foto());
+        Assertions.assertEquals(egresso.getNome(), retorno.getNome());
+        Assertions.assertEquals(egresso.getEmail(), retorno.getEmail());
+        Assertions.assertEquals(egresso.getCpf(), retorno.getCpf());
+        Assertions.assertEquals(egresso.getResumo(), retorno.getResumo());
+        Assertions.assertEquals(egresso.getUrl_foto(), retorno.getUrl_foto());
 
         // rollback
         repo.delete(retorno);
     }
+
+    @Test
+    public void deveRemoverEgresso() {
+        //cenário
+        Egresso egresso = Egresso.builder()
+                .nome("tuludan")
+                .email("a@a.com")
+                .cpf("1234")
+                .resumo("lorem ipsum lore")
+                .url_foto("teste")
+                .build();
+        
+        //ação
+        Egresso salvo = repo.save(egresso);  //salva
+        Long id = salvo.getId();
+        repo.deleteById(id);
+        
+        
+        //verificação
+        Optional<Egresso> temp = repo.findById(id);        
+        Assertions.assertFalse(temp.isPresent());
+    }
+
+    @Test
+    public void deveObterPorNomeEgresso(){
+        //cenário
+        String nome = "tuludan";
+        Egresso egresso = Egresso.builder()
+                .nome(nome)
+                .email("a@a.com")
+                .cpf("1234")
+                .resumo("lorem ipsum lore")
+                .url_foto("teste")
+                .build();
+        
+        // ação
+        Egresso retorno = repo.save(egresso); //salvar
+        Egresso consulta = repo.obterEgressoPorNome(nome); //consulta
+
+        //verificação
+        Assertions.assertNotNull(consulta);
+        Assertions.assertEquals(egresso.getNome(), consulta.getNome());
+        Assertions.assertEquals(egresso.getEmail(), consulta.getEmail());
+        Assertions.assertEquals(egresso.getCpf(), consulta.getCpf());
+        Assertions.assertEquals(egresso.getResumo(), consulta.getResumo());
+        Assertions.assertEquals(egresso.getUrl_foto(), consulta.getUrl_foto()); 
+        
+        //rollback
+        repo.delete(retorno);
+        
+    }
+
+    
 
 }
