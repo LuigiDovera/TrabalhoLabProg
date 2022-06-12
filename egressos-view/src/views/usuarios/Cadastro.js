c5da872df9b681771042860496c71fe1a33b6709
Daniel Moreira Pinto <danielmoreirapinto.ti@gmail.com>
2022-06-12 16:56:02 -0300

    [Dev] Daniel - Cadastro - Incluidas variáveis dos campos do formulário, InputNumber age estranhamente

--- egressos-view/src/views/usuarios/Cadastro.js
+++ egressos-view/src/views/usuarios/Cadastro.js
@@ -7,6 +7,17 @@ import { Password } from 'primereact/password';
 import { Button } from 'primereact/button';
 
 class Cadastro extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            nome: null,
+            cpf: null,
+            email: null,
+            resumo: null,
+            senha: null
+        };
+    }
+
     render() {
         return (
             <div className={styles.mainDiv}>
@@ -17,23 +28,33 @@ class Cadastro extends React.Component {
 
                     <div className={styles.formDiv}>
                         <span className={`${styles.formItemSpan} p-float-label`}>
-                            <InputText id="inputnome" className="w-full" />
+                            <InputText id="inputnome" className="w-full"   
+                                value={this.state.nome} 
+                                onChange={(e) => this.setState({ nome: e.target.value })} />
                             <label htmlFor="inputnome">Nome Completo</label>
                         </span>
                         <span className={`${styles.formItemSpan} p-float-label`}>
-                            <InputNumber id="inputcpf" mode="decimal" useGrouping={false} className="w-50" />
+                            <InputNumber id="inputcpf" mode="decimal" useGrouping={false} className="w-50"   
+                                value={this.state.cpf} 
+                                onChange={(e) => this.setState({ cpf: e.target.value })} />
                             <label htmlFor="inputtext">CPF</label>
                         </span>
                         <span className={`${styles.formItemSpan} p-float-label`}>
-                            <InputText id="email" type="text" className="w-full" />
+                            <InputText id="email" type="text" className="w-full"   
+                                value={this.state.email} 
+                                onChange={(e) => this.setState({ email: e.target.value })} />
                             <label htmlFor="email">Email</label>
                         </span>
                         <span className={`${styles.formItemSpan} p-float-label`}>
-                            <InputTextarea id="textresumo" rows={3} className="w-full" />
+                            <InputTextarea id="textresumo" rows={3} className="w-full"   
+                                value={this.state.resumo} 
+                                onChange={(e) => this.setState({ resumo: e.target.value })} />
                             <label htmlFor="textresumo">Resumo</label>
                         </span>
                         <span className={`${styles.formItemSpan} p-float-label`}>
-                            <Password id="inputsenha"  className="w-full" />
+                            <Password id="inputsenha"  className="w-full"   
+                                value={this.state.senha} 
+                                onChange={(e) => this.setState({ senha: e.target.value })} />
                             <label htmlFor="inputsenha">Senha</label>
                         </span>
                         <Button label="Cadastrar" icon="pi pi-user" className="w-full" />
